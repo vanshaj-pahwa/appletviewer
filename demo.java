6ef8fdbebd551c6dc9e0bd1a8264d939a354c098
Vanshaj Pahwa <vanshajpahwa07@gmail.com>
2020-10-01 00:43:18 +0530

    second commit

---
+++ demo.java
@@ -0,0 +1,20 @@
+import java.applet.Applet;
+import java.awt.*;
+
+/* <applet code="demo.class" width="200" height="200"></applet>
+*/
+
+public class demo extends Applet {
+   public void paint(Graphics g){
+      g.setColor(Color.yellow);
+      g.fillOval(20,20,150,150);
+      g.setColor(Color.black);
+      g.fillOval(50,60,15,25);
+      g.fillOval(120,60,15,25);
+      int x[] = {95,85,106,95};
+      int y[] = {85,104,104,85};
+      g.drawPolygon(x, y, 4);
+      g.drawArc(55,95,78,50,0,-180);
+   }
+}
+
